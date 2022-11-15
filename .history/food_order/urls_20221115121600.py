3374d8ee5e352deb4e9b8b6d23dbeb2ca4f93212
Mohsen Motamedipour <m.motamedipour2022@yahoo.com>
2022-11-15 12:16:31 +0330

    add templates

--- .history/food_order/urls_20221115121600.py
+++ .history/food_order/urls_20221115121600.py
@@ -18,6 +18,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("",views.home,name='home'),
+    path("",views.home,name=),
     path("admin/", admin.site.urls),
 ]
--- .history/food_order/urls_20221115121604.py
+++ .history/food_order/urls_20221115121604.py
@@ -18,6 +18,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("",views.home,name='home'),
+    path("",views.home,name=ho),
     path("admin/", admin.site.urls),
 ]
